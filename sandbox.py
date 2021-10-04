fa1baaf1f6905c722ae2e27fc2d5a6f114790269
cssp-user <88207481+cssp-user@users.noreply.github.com>
2021-10-05 00:57:02 +0530

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,17 +1,6 @@
-import subprocess
-
-cmds = ["sudo yum install git -y && sudo yum install wget -y && sudo yum install curl -y && sudo yum install unzip -y",
-"sudo wget https://releases.hashicorp.com/terraform/0.14.10/terraform_0.14.10_linux_amd64.zip && sudo unzip terraform_0.14.10_linux_amd64.zip && sudo mv terraform /bin/ && sudo rm -rf ./terraform*",
-"sudo curl 'https://s3.amazonaws.com/aws-cli/awscli-bundle.zip' -o 'awscli-bundle.zip' && sudo unzip awscli-bundle.zip && sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws && sudo rm -rf ./awscli-bundle*"]
-
-for cmd in cmds:
-    print("command name", cmd)
-    cmd_execution = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True)
-    cmd_output, cmd_error = cmd_execution.communicate()
-    print("output of the command", cmd_output)
-
-
-#!/usr/bin/env python
-import os
-cmd = "aws configure"
-os.system(cmd)
+sudo yum install git -y && sudo yum install wget -y && sudo yum install unzip -y
+sudo wget https://releases.hashicorp.com/terraform/0.14.10/terraform_0.14.10_linux_amd64.zip && sudo unzip terraform_0.14.10_linux_amd64.zip && sudo mv terraform /bin/ && sudo rm -rf ./terraform*
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
+aws configure
