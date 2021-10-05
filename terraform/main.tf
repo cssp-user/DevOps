ab4e21467cbaf5b17e64bae2491a8dcaa4f3197c
cssp-user <88207481+cssp-user@users.noreply.github.com>
2021-10-05 12:53:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,6 @@ source = "./modules/instances"
 
 provider "aws"{
 # region = "${var.myregion}"
-shared_credentials_file = "/home/centos/.aws/credentials"
+#shared_credentials_file = "/home/centos/.aws/credentials"
 }
 
