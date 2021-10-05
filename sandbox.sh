5246394846e7cd70a65224c9bb7965bbaec4a8d8
cssp-user <88207481+cssp-user@users.noreply.github.com>
2021-10-05 13:10:22 +0530

    Update sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -1,5 +1,4 @@
 sudo yum install git -y && sudo yum install wget -y && sudo yum install unzip -y
-sudo wget https://releases.hashicorp.com/terraform/0.14.10/terraform_0.14.10_linux_amd64.zip && sudo unzip terraform_0.14.10_linux_amd64.zip && sudo mv terraform /bin/ && sudo rm -rf ./terraform*
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
